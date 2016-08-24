befc00b1eade0f33d7d93ff12ecbb9e95ebc59dd
Jacopo Francesco Zemella <jacopofrancesco.zemella@studenti.unimi.it>
2016-08-24 17:51:02 +0200

    Cambio tipo Matrici da [[a]] in [[Double]]
    Ho notato che il tipo Double contiene tutte le specifiche che mi
    servono, tra cui Fractional e NFData. Poichè di fatto è ragionevole
    considerare i Double come numeri reali (quale è il nostro caso) è
    probabile che sarà questa la configurazione effettiva delle matrici da
    qui in poi.

--- Codice/AlgebraLineare/Matrix.hs
+++ Codice/AlgebraLineare/Matrix.hs
@@ -6,43 +6,41 @@ import System.IO
 import Control.Parallel
 import Control.Parallel.Strategies
 	
+makeMat :: Int -> [[Double]]
+makeMat n = replicate n [1.0..t] where t = fromIntegral n
 
-makeMat n = replicate n [1..n]
 
-zeroes n = replicate n (replicate n 0)
-
-identity :: Num a => Int -> [[a]]
+identity :: Int -> [[Double]]
 identity n = identity2 n 0
 
-identity2 :: Num a => Int -> Int -> [[a]]
-
+identity2 :: Int -> Int -> [[Double]]
 identity2 n m 
 	| n == m = []
 	| otherwise = [((replicate m 0) ++ [1] ++ (replicate (n-m-1) 0))] ++ (identity2 n (m+1))  
 
 --operazioni sequenziali tra matrici
 
-sumVector :: Num a => [a] -> [a] -> [a]
+sumVector :: [Double] -> [Double] -> [Double]
 sumVector v1 v2 = [a + b | a <- v1, b <- v2]
 
-subVector :: Num a => [a] -> [a] -> [a]
+subVector :: [Double] -> [Double] -> [Double]
 subVector v1 v2 = [a - b | a <- v1, b <- v2]
 					  
-scalar :: Num a => [a] -> [a] -> a
+scalar :: [Double] -> [Double] -> Double
 scalar a b = sum (zipWith (*) a b)
 
 
-sumMatrix :: Num a => [[a]] -> [[a]] -> [[a]]
+sumMatrix :: [[Double]] -> [[Double]] -> [[Double]]
 sumMatrix = zipWith (zipWith (+))
 
-subMatrix :: Num a => [[a]] -> [[a]] -> [[a]]
+subMatrix :: [[Double]] -> [[Double]] -> [[Double]]
 subMatrix = zipWith (zipWith (-))
 
-prodMatrix :: Num a => [[a]] -> [[a]] -> [[a]]
+prodMatrix :: [[Double]] -> [[Double]] -> [[Double]]
 prodMatrix m1 m2 = [[scalar a b | b <- column] | a <- m1]
                  where column = transpose m2
 
-powMatrix :: Num a => [[a]] -> Int -> [[a]]
+powMatrix :: [[Double]] -> Int -> [[Double]]
 powMatrix m 0 = (identity dim) where dim = length m
 powMatrix m 1 = m
 powMatrix m n = prodMatrix m (powMatrix m (n-1))
@@ -50,18 +48,18 @@ powMatrix m n = prodMatrix m (powMatrix m (n-1))
 	
 --operazioni parallele tra matrici
 	
-sumMatPar :: (NFData a, Num a) => [[a]] -> [[a]] -> [[a]]
+sumMatPar :: [[Double]] -> [[Double]] -> [[Double]]
 sumMatPar a b = (sumMatrix a b) `using` parList rdeepseq
 
-subMatPar :: (NFData a, Num a) => [[a]] -> [[a]] -> [[a]]
+subMatPar :: [[Double]] -> [[Double]] -> [[Double]]
 subMatPar a b = (subMatrix a b) `using` parList rdeepseq
 	  
-prodMatPar :: (NFData a, Num a) => [[a]] -> [[a]] -> [[a]]
+prodMatPar :: [[Double]] -> [[Double]] -> [[Double]]
 prodMatPar a b = (prodMatrix a b) `using` parList rdeepseq
 
 
 				  
-powMatPar :: (NFData a, Num a) => [[a]] -> Int -> [[a]]
+powMatPar :: [[Double]] -> Int -> [[Double]]
 powMatPar m 0 = (identity dim) where dim = length m
 powMatPar m 1 = m
 powMatPar m n = prodMatPar m ris
@@ -71,33 +69,32 @@ powMatPar m n = prodMatPar m ris
 				  
 --determinante
 
-deleteColumn :: [[a]] -> Int -> [[a]]
+deleteColumn :: [[Double]] -> Int -> [[Double]]
 deleteColumn [] _ = error "Error input Matrix"
 deleteColumn m col = a ++ b where (a, _:b) = splitAt col m
 	
-deleteElement :: [a] -> Int -> [a]
+deleteElement :: [Double] -> Int -> [Double]
 deleteElement x index = left ++ right where (left, _:right) = splitAt index x
 
-deleteRow :: [[a]] -> Int -> [[a]]
+deleteRow :: [[Double]] -> Int -> [[Double]]
 deleteRow [] _ = error "Error input Matrix"
 deleteRow m row = [deleteElement x row | x <- m]
 						
 
-minor :: (Num a, Fractional a) => [[a]] -> Int -> Int -> [[a]]
+minor :: [[Double]] -> Int -> Int -> [[Double]]
 minor [] _ _ = error "Error input Matrix"
 minor m row col = deleteRow m1 row where m1 = (deleteColumn m col)
 
-det :: (Num a, Fractional a) => [[a]] -> a
+det :: [[Double]] -> Double
 det [] = error "Error input Matrix"
-det [[a]] = a
+det [[x]] = x
 det m = sum [a*s*(det m1) | i <- [0..dim-1], let a = (head m !! i), let m1 = (minor m i 0), let s = (-1)^i] 
 	where
 		dim = length m
-		
-		
+
 --matrice inversa
 
-invert :: (Num a, Fractional a) => [[a]] -> [[a]]
+invert :: [[Double]] -> [[Double]]
 invert [[]] = [[]]
 invert m = transpose [[s*(det m1)/d | i <- [0..dim-1], let m1 = (minor m i j), let s = (-1)^(i+j)] | j <- [0..dim-1]]
 		where
